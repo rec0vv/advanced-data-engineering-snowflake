7f9b0176140feea9852053a3b856dc2bad62071e
Aman <aman_ankit@yahoo.com>
2026-04-18 16:18:28 +0530

    Fixes in the codes for table country

--- module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
+++ module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
@@ -58,13 +58,13 @@ raw zone table build
 -- todo: complete table build
 CREATE OR ALTER TABLE {{env}}_tasty_bytes.raw_pos.country
 (
-   country_id,
-   country,
-   iso_currency,
-   iso_country,
-   city_id,
-   city,
-   city_population
+   country_id NUMBER(18,0),
+   country VARCHAR(16777216),
+   iso_currency VARCHAR(3),
+   iso_country VARCHAR(2),
+   city_id NUMBER(19,0),
+   city VARCHAR(16777216),
+   city_population VARCHAR(16777216)
 );
 
 
